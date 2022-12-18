3aed64dc2e2aa17beb7957930334b6f8bc63b38c
Thilo Ognibeni <thilo@ognibeni.info>
2022-12-18 11:17:04 +0100

    Initial commit

---
+++ src/main/kotlin/info/ognibeni/club/score/ScoreServiceApplication.kt
@@ -0,0 +1,11 @@
+package info.ognibeni.club.score
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScoreServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<ScoreServiceApplication>(*args)
+}
---
+++ src/test/kotlin/info/ognibeni/club/score/ScoreServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package info.ognibeni.club.score
+
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScoreServiceApplicationTests {
+
+	// FIXME: Move to Integration test
+//	@Test
+	fun contextLoads() {
+	}
+
+}
